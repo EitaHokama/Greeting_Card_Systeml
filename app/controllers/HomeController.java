e322b177122f809d4510dc5736e92accdb88a46f
iTAP <iTAP@iTAP-PC13>
2016-05-31 17:44:57 +0900

    ビュー終了後

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,18 +1,27 @@
 package controllers;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.avaje.ebean.Update;
+
 import models.CaseStudy;
 import models.Employees;
+import models.Department;
 import models.Gratitude_Card;
+import play.data.FormFactory;
 import play.mvc.*;
-import java.util.Map;
 import views.html.*;
+
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
-
+	@Inject
+	private FormFactory formFactory;
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
@@ -50,13 +59,30 @@ public class HomeController extends Controller {
 
 
     public Result createkanri() {
-        // java.util.Map をインポートしておく
-        Map<String, String[]> params = request().body().asFormUrlEncoded();
-        Employees newEmp = new Employees();
-        newEmp.name = params.get("name")[0]; // <input type="text" name="name" /> に入力された値
-        newEmp.save();
+    	Map<String, String[]> params = request().body().asFormUrlEncoded();
+
+//    	Employees viewEmployees = formFactory.form(Employees.class).bindFromRequest().get();
+    	Department dep = new Department();
+    	dep.department_id = Integer.valueOf(params.get("departrment_id")[0]);
+
+    	Employees selectEmployees = Employees.find.byId(Integer.valueOf(params.get("employees_id")[0]));
+//    	Employees selectEmployees = Employees.find.byId(viewEmployees.employees_id);
+    	selectEmployees.del_flag = 1;
+    	selectEmployees.update();
+
+    	selectEmployees.employees_id = null;
+    	selectEmployees.name = params.get("name")[0];
+//    	selectEmployees.name = viewEmployees.name;
+    	selectEmployees.depatrment_id = dep;
+    	selectEmployees.pass = "0000";
+    	selectEmployees.insert();
+
         return redirect(routes.HomeController.kanri());
+
+
     }
+
+
 
     }
 
--- playDebug.bat
+++ playDebug.bat
@@ -1,1 +1,2 @@
+activator Eclipse
 activator -Dhttp.port=9000 -jvm-debug 9999 h2-browser run
